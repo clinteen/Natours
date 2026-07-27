f9edaf536643376d4fd6fc7922714f8f8aa1c855
Fyne Clinton <clintonfyne1@gmail.com>
2026-07-27 22:31:42 +0100

    Fixed line items bug

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -67,9 +67,17 @@ exports.bookingSession = catchAsync(async (req, res, next) => {
 });
 
 const createBookingWebHook = async (session) => {
+    const fullSession = await stripe.checkout.sessions.retrieve(session.id, {
+        expand: ['line_items', 'customer']
+    });
+
     const tour = session.client_reference_id;
     const user = User.findOne({ email: session.customer_email }).id;
-    const price = session.line_items[0].price_data.unit_amount / 100;
+    // const price = session.line_items[0].price_data.unit_amount / 100;
+    const lineItems = fullSession.line_items.data;
+    const price = lineItems[0].amount_total / 100;
+
+    console.log(fullSession);
 
     const selectedDate = session.metadata.startDate;
     const selectedTour = Tour.findById(tour);
